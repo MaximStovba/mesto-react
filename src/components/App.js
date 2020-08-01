4461c19308ac93e72bdd11226fb9732d1549fa9b
Maxim Stovba <maxim.stovba@gmail.com>
2020-08-01 12:01:29 +0300

    setCards

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,6 @@ import PopupWithForm from './PopupWithForm';
 import EditProfilePopup from './EditProfilePopup';
 import EditAvatarPopup from './EditAvatarPopup';
 import AddPlacePopup from './AddPlacePopup';
-// import InputAvatarForm from './InputAvatarForm';
-// import InputAddForm from './InputAddForm';
-// import InputEditForm from './InputEditForm';
 import ImagePopup from './ImagePopup';
 import Footer from './Footer';
 // импортируем api
@@ -18,7 +15,6 @@ import { api } from '../utils/Api';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function App() {
-
   // ------------- CARDS ----------------
   // Переменные состояния >
   const [cards, setCards] = React.useState([]);
@@ -92,18 +88,22 @@ function App() {
       name: card.name,
     });
   }
+
   // обработчик открытия попапа "редактирования профиля"
   function handleEditProfileClick() {
     setIsEditProfilePopupOpen(true);
   }
+
   // обработчик открытия попапа "добавления новой карточки"
   function handleAddPlaceClick() {
     setIsAddPlacePopupOpen(true);
   }
+
   // обработчик открытия попапа "смены аватара"
   function handleEditAvatarClick() {
     setIsEditAvatarPopupOpen(true);
   }
+
   // обработчик закрытия всех попапов
   function closeAllPopups() {
     setIsEditProfilePopupOpen(false);
@@ -111,6 +111,7 @@ function App() {
     setIsEditAvatarPopupOpen(false);
     setSelectedCard(false);
   }
+
   // обработчик обновления информации пользователя
   function handleUpdateUser(formData) {
     api.patchUserInfo(formData)
@@ -139,8 +140,7 @@ function App() {
   function handleAddPlace(formData) {
     api.postNewCard(formData)
     .then((newCard) => {
-      // newCard
-      setCards([...cards, newCard]);
+      setCards([newCard, ...cards]);
     })
     .catch((err) => {
       console.log('Ошибка. Запрос не выполнен: ', err);
